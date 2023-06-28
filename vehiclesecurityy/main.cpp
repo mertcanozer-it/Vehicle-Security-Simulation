65660698bbd0385ac3e5b590bfd5eccdb46184a1
mertcanozer-it <mertcanmakiozer@outlook.de>
2023-06-28 13:53:50 +0900

    Initial Commit

---
+++ vehiclesecurityy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  vehiclesecurityy
+//
+//  Created by Mertcan Ozer on 2023/06/28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
